b9b8acac17359330419f534206640d9e18e6d94b
Masaki Nishimoto <m.nishimoto@kaleidoscope.co.jp>
2025-09-02 12:32:32 +0900

    微調整

--- functions.php
+++ functions.php
@@ -1,5 +1,5 @@
 <?php
 add_action('wp_enqueue_scripts', function(){
-  wp_enqueue_style('parent-style', get_template_directory_uri() . '/style.css');
+  wp_enqueue_style('parent-style', get_template_directory_uri().'/style.css');
 });
 ?>
